7c46650438831250a61d10be2ee0e25842f9ca25
nathaliesainz <nathaliesainz13@gmail.com>
2023-04-03 13:35:04 -0600

    Add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,56 @@
 import { useState } from 'react'
+import styled from '@emotion/styled'
+import CryptoImage from './img/crypto-image.png'
+
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`
+
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: #FFF;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
+`
+
+const Image = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`
 
 function App() {
 
   return (
-    <h1>From App</h1>
+    <Container>
+      <Image
+        src={CryptoImage}
+        alt="Crypto Image"
+      />
+      <div>
+        <Heading>Quote Price Cryptocurrency</Heading>
+      </div>
+    </Container>
   )
 }
 
