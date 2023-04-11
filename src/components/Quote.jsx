1677d09e1e0e915ce448c852416a410b2529d95b
nathaliesainz <nathaliesainz13@gmail.com>
2023-04-10 20:10:17 -0600

    Add styles to quote

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,15 +1,47 @@
 import styled from "@emotion/styled"
 
+const Container = styled.div`
+  color: #FFF;
+  font-family: 'Lato', sans-serif;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 30px;
+`
+
+const Image = styled.img`
+  display: block;
+  width: 120px;
+`
+const Text = styled.p`
+   font-size: 18px;
+   span {
+    font-weight: 700;
+  }
+`
+const Price = styled.p`
+  font-size: 25px;
+  span {
+    font-weight: 700;
+  }
+`
+
 const Quote = ({quote}) => {
   const {PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } = quote
   return (
-    <div>
-        <p>Price is: <span>{PRICE}</span></p>
-        <p>Highest price of the day: <span>{HIGHDAY}</span></p>
-        <p>Lowest price of the day: <span>{LOWDAY}</span></p>
-        <p>Change in the last 24 hours: <span>{CHANGEPCT24HOUR}</span></p>
-        <p>Last update: <span>{LASTUPDATE}</span></p>
-    </div>
+    <Container>
+      <Image 
+        src={`https://cryptocompare.com/${IMAGEURL}`} 
+        alt="Crypto Image" 
+      />
+        <div>
+          <Price>Price is: <span>{PRICE}</span></Price>
+          <Text>Highest price of the day: <span>{HIGHDAY}</span></Text>
+          <Text>Lowest price of the day: <span>{LOWDAY}</span></Text>
+          <Text>Change in the last 24 hours: <span>{CHANGEPCT24HOUR}</span></Text>
+          <Text>Last update: <span>{LASTUPDATE}</span></Text>
+        </div>
+    </Container>
   )
 }
 export default Quote
